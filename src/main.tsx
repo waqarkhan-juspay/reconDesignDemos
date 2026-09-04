f9fb95c3467bfddbc8b284b69fdf17c9dd2f03e4
Waqar Khan <waqar.khan@Waqar-Khan-K44WCMXF5H.local>
2026-09-04 20:11:17 +0530

    The initiation.

---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+import { ThemeProvider } from '@juspay/blend-design-system'
+import '@juspay/blend-design-system/style.css'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
+  </StrictMode>,
+)
